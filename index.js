e76dcd4c72a59590fc6c925cba344a0bfae6f0ec
Alex Maslovsky <alexey.maslovsky@satelliteinnovations.io>
2022-05-15 15:20:35 +0300

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+import App from './src/App.js';
+
+const resizeCanvas = (canvas) => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  });
+};
+
+const render = (rootContext, targetContext) => {
+  rootContext.clearRect(0, 0, rootContext.canvas.width, rootContext.canvas.height);
+
+  const targetWidth = targetContext.canvas.width * (rootContext.canvas.height / targetContext.canvas.height);
+
+  rootContext.drawImage(
+    targetContext.canvas,
+    rootContext.canvas.width / 2 - targetWidth / 2,
+    0,
+    targetWidth,
+    rootContext.canvas.height,
+  );
+
+  requestAnimationFrame(() => render(rootContext, targetContext));
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const rootCanvas = document.createElement('canvas');
+  resizeCanvas(rootCanvas);
+
+  const rootContext = rootCanvas.getContext('2d');
+
+  if (!rootContext) {
+    throw new Error('Canvas is not supported');
+  }
+
+  const targetContext = await App(rootContext);
+
+  document.body.prepend(rootCanvas);
+
+  render(rootContext, targetContext);
+});
---
+++ src/App.js
@@ -0,0 +1,82 @@
+import GifPlayer from './GifPlayer.js';
+
+const random = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+const randomFromSource = (...source) => {
+  return source[random(source.length)];
+};
+
+const loadGifs = () => {
+  const gifCount = 15;
+
+  const promises = Array.from({ length: gifCount }).map((_, index) => {
+    return new Promise((resolve, reject) => {
+      const gif = new GifPlayer();
+
+      gif.load(`/images/${index + 1}.gif`);
+
+      gif.onload = () => {
+        resolve(gif);
+      };
+    });
+  });
+
+  return Promise.all(promises);
+};
+
+const generateRandomImages = (gifs, x, y, width, height) => {
+  return Array.from({ length: 1 }).map((_, index) => {
+    return {
+      gif: randomFromSource(...gifs),
+      x: x + random(width),
+      y: y + random(height),
+    };
+  });
+};
+
+const generateImages = (gifs, context) => {
+  const step = 10;
+  const stepX = context.canvas.width / step;
+  const stepY = context.canvas.height / step;
+
+  const map = Array.from({ length: step + 1 }).map((_, i) => {
+    return Array.from({ length: step }).map((_, j) => {
+      return generateRandomImages(gifs, stepX * j, stepY * (i - 1), stepX, stepY);
+    });
+  });
+
+  return map.flat(2);
+};
+
+const render = (context, images) => {
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+  images.map(({ gif, x, y }) => {
+    context.drawImage(gif.image, x, y);
+  });
+
+  requestAnimationFrame(() => render(context, images));
+};
+
+const App = async () => {
+  const canvas = document.createElement('canvas');
+  canvas.width = 3000;
+  canvas.height = 1080;
+  const context = canvas.getContext('2d');
+
+  if (!context) {
+    throw new Error('Canvas is not supported');
+  }
+
+  const gifs = await loadGifs();
+
+  const images = generateImages(gifs, context);
+
+  requestAnimationFrame(() => render(context, images));
+
+  return context;
+};
+
+export default App;
